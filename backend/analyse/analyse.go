7ef394370db6454b36e3b607ca37ca59eb427aed
Ivan Kapelyukh <ivan.kapelyukh@gmail.com>
2020-02-09 00:34:19 +0000

    Implement normalisation

--- backend/analyse/analyse.go
+++ backend/analyse/analyse.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
-	rse, _ := get_rse([]float64{0.5, 0.6}, []float64{0.5, 0.7})
+	a := []float64{5, 6}
+	b := []float64{5, 7}
+	rse, _ := get_rse(normalise(a), normalise(b))
 	fmt.Printf("%v", rse)
 }
 
@@ -14,7 +16,7 @@ func main() {
 // Input values should be sorted and between 0 and 1
 func get_rse(a []float64, b []float64) (rse float64, err error) {
 	if len(a) != len(b) {
-		return -1, fmt.Errorf("Need same number of points to calculate error")
+		return -1, fmt.Errorf("need same number of points to calculate error")
 	}
 
 	for i := range a {
@@ -22,4 +24,15 @@ func get_rse(a []float64, b []float64) (rse float64, err error) {
 	}
 
 	return math.Sqrt(rse / float64(len(a))), nil
+}
+
+// Pre: points is sorted, e.g. 3, 15, 67
+func normalise(points []float64) []float64 {
+	normalised := make([]float64, len(points))
+	duration := points[len(points)-1]
+	for i := range points {
+		normalised[i] = points[i] / duration
+	}
+
+	return normalised
 }
