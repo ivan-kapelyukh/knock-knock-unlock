68eecd27894b1f2dae4ac39355039a8b1cdd632b
Pablo Gamito <pablo@gamito.me>
2020-02-09 11:26:51 +0000

    Remove references to record

--- web/js/index.js
+++ web/js/index.js
@@ -3,7 +3,6 @@ import { login } from "./client.js";
 import Waves from "./waves.js";
 
 let waves = new Waves();
-let record = new Record();
 
 let knockRecorder = new KnockRecorder();
 knockRecorder.add_knock_listener(waves.knock.bind(waves));
@@ -14,14 +13,10 @@ document.getElementById("start").addEventListener("click", function() {
   console.log("Recording...");
   waves.play();
   knockRecorder.start();
-
-  record.start();
 });
 
 document.getElementById("stop").addEventListener("click", function() {
   let knocks = knockRecorder.stop();
   console.log(knocks);
   login("test3", knocks).then(d => console.log(d.text()));
-
-  record.stop();
 });
